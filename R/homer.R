fc2ebba602f508aae2cfe047fedda30dffba484d
Bill Zichos <billzichos@hotmail.com>
2017-11-21 20:48:55 -0500

    Output format as parameter to home_estimate()
    User has the ability to choose whether data is presented as XML or as an R
    List object.  The List is preferred in you plan on working with the data
    interactively.  However, if simply archiving, XML is probably the way to
    go.

--- R/homer.R
+++ R/homer.R
@@ -3,25 +3,27 @@
 #' \code{home_estimate} returns data for one or more homes listed on Zillow.
 #'
 #' @param x A character string representing a single Zillow ID
-#' @param zws_id A character string representing a your unique Zillow Web
+#' @param zws_path A character string representing a your unique Zillow Web
 #'           Services key.
+#' @param format Specifies whether the return object is in XML or List format.
+#'         Defaults to list.
 #'
 #' @details Future versions will include the ability to pass in more than one
 #'   property.
 #'
-#' @details Link to zillow web services where users obtain an account.
-#'
-#' @details Future versions will have different output options.
-#'
 #' @return XML from Zillow Web Service.  Details of the XML are maitained by
 #' Zillow here - https://www.zillow.com/howto/api/GetZestimate.htm.
 #'
-#' @example home_estimate("36086728")
+#' @examples home_estimate("36086728")
 #'
 #' @export
-home_estimate <- function(x, zws_path="~/zillow_key.txt") {
+home_estimate <- function(x, zws_path="~/zillow_key.txt", format="list") {
     zws_id <- readLines(zws_path)
     base_URL <- "http://www.zillow.com/webservice/GetZestimate.htm?"
     URL <- paste0(base_URL,"zws-id=",zws_id,"&zpid=",x)
-    xml2::as_list(xml2::read_xml(URL))
+    if (format=="list") {
+        xml2::as_list(xml2::read_xml(URL))
+    } else if (format=="xml") {
+        xml2::read_xml(URL)
+    } else print("Unknown format.  Expecting 'list' or 'xml.'")
 }
